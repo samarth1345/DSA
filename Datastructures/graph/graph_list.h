dfd205f340a3955afb31a7e76c886589aa52c3cc
Samarth Babbar <bsamarth56@gmail.com>
2021-12-21 18:06:50 +0530

    //

--- Datastructures/graph/graph_list.h
+++ Datastructures/graph/graph_list.h
@@ -4,8 +4,7 @@ using namespace std;
 class graph
 {
     int no_of_vertices;
-    vector<int> Matrix[MAX];
-
+    vector<int> List[MAX];
 public:
     graph(int val)
     {
@@ -13,17 +12,21 @@ public:
     }
     void input(int vertice1, int vertice2)
     {
-        Matrix[vertice1-1].push_back(vertice2);
-        Matrix[vertice2-1].push_back(vertice1);
+        List[vertice1].push_back(vertice2);
+        List[vertice2].push_back(vertice1);
     }
     void print()
     {
-        for(int i=0;i<no_of_vertices;i++)
+        for(int i=1;i<=no_of_vertices;i++)
         {
-            cout<<i+1<<": ";
-            for(int j=0;j<Matrix[i].size();j++)
-                cout<<Matrix[i][j]<<" ";
+            cout<<i<<": ";
+            for(int j=0;j<List[i].size();j++)
+                cout<<List[i][j]<<" ";
             cout<<endl;
         }
+    }
+    vector<int> *getList()
+    {
+        return List;
     }
 };
