6c0120683a2da1510c4033378ff51f1c97891d71
Samarth Babbar <bsamarth56@gmail.com>
2022-01-27 14:43:47 +0530

    //

---
+++ Datastructures/graph/island_perimeter.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+void spread(vector<vector<int>> &grid, vector<vector<int>> &visited, int x, int y, int &peri)
+{
+    if (grid[x][y] == 0 || visited[x][y] == 1)
+        return;
+    visited[x][y] = 1;
+    if (x == grid.size() - 1)
+        peri++;
+    if (x == 0)
+        peri++;
+    if (y == grid[0].size() - 1)
+        peri++;
+    if (y == 0)
+        peri++;
+    if (x + 1 < grid.size())
+    {
+        peri += ((grid[x + 1][y] == 0) ? 1 : 0);
+        spread(grid, visited, x + 1, y, peri);
+    }
+    if (x - 1 >= 0)
+    {
+        peri += ((grid[x - 1][y] == 0) ? 1 : 0);
+        spread(grid, visited, x - 1, y, peri);
+    }
+    if (y + 1 < grid[x].size())
+    {
+        peri += ((grid[x][y + 1] == 0) ? 1 : 0);
+        spread(grid, visited, x, y + 1, peri);
+    }
+    if (y - 1 >= 0)
+    {
+        peri += ((grid[x][y - 1] == 0) ? 1 : 0);
+        spread(grid, visited, x, y - 1, peri);
+    }
+}
+int islandPerimeter(vector<vector<int>> &grid)
+{
+    vector<vector<int>> visited;
+    int peri = 0;
+    for (int i = 0; i < grid.size(); i++)
+    {
+        vector<int> temp(grid[i].size(), 0);
+        visited.push_back(temp);
+    }
+    for (int i = 0; i < grid.size(); i++)
+    {
+        for (int j = 0; j < grid[i].size(); j++)
+        {
+            if (grid[i][j] == 1)
+            {
+                spread(grid, visited, i, j, peri);
+                break;
+            }
+        }
+    }
+    return peri;
+}
