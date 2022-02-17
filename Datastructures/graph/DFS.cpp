c4ed177ed6e7ef09993e3d1da7e2de77db6103b9
Samarth Babbar <bsamarth56@gmail.com>
2022-02-18 02:57:02 +0530

    //

--- Datastructures/graph/DFS.cpp
+++ Datastructures/graph/DFS.cpp
@@ -2,42 +2,74 @@
 using namespace std;
 #include "graph_list.h"
 using namespace std;
-void DFS_help(vector<int> *List,vector<int> &visited,int t)
+double dodfs(vector<vector<pair<string, double>>> List, unordered_map<string, int> getIndex, vector<int> &visited, string start, string end, int index)
 {
-    for (int j = 0; j < List[t].size(); j++)
+    if (start == end)
+        return 1;
+    for (int i = 0; i < List[index].size(); i++)
     {
-        if (!visited[List[t][j]])
+        int temp_index = getIndex[List[index][i].first];
+        if (visited[temp_index] == 0)
         {
-            cout << List[t][j] << " ";
-            visited[List[t][j]] = 1;
-            DFS_help(List,visited,List[t][j]);
+            string newstart = List[index][i].first;
+            visited[temp_index] = 1;
+            double ans = dodfs(List, getIndex, visited, newstart, end, temp_index);
+            if (ans != -1)
+                return ans * List[index][i].second;
         }
     }
+    return -1;
 }
-void DFS(vector<int> *List, int vertices)
+double dfs(vector<vector<pair<string, double>>> List, unordered_map<string, int> getIndex, string start, string end, int index)
 {
-    vector<int> visited(vertices + 1);
-    for (int i = 1; i <= vertices; i++)
+    if (start == end && getIndex.find(start) != getIndex.end())
+        return 1;
+    vector<int> visited(List.size(), 0);
+    visited[index] = 1;
+    for (int i = 0; i < List[index].size(); i++)
     {
-        if (!visited[i])
+        int temp_index = getIndex[List[index][i].first];
+        if (visited[temp_index] == 0)
         {
-            cout << i << " ";
-            visited[i] = 1;
-            DFS_help(List,visited,i);
+            string newstart = List[index][i].first;
+            double ans = dodfs(List, getIndex, visited, newstart, end, temp_index);
+            visited[temp_index] = 1;
+            if (ans != -1)
+                return ans * List[index][i].second;
         }
     }
+    return -1;
 }
-int main()
+vector<double> calcEquation(vector<vector<string>> &equations, vector<double> &values, vector<vector<string>> &queries)
 {
-    int n, m;
-    cin >> n >> m;
-    graph *obj = new graph(n);
-    for (int i = 0; i < m; i++)
+    vector<vector<pair<string, double>>> List;
+    unordered_map<string, int> getIndex;
+    int size = equations.size();
+    for (int i = 0; i < size; i++)
     {
-        int u, v;
-        cin >> u >> v;
-        obj->input(u, v);
+        if (getIndex.find(equations[i][0]) == getIndex.end())
+        {
+            List.push_back({make_pair(equations[i][1], values[i])});
+            getIndex[equations[i][0]] = List.size() - 1;
+        }
+        else
+            List[getIndex[equations[i][0]]].push_back({equations[i][1], values[i]});
+        if (getIndex.find(equations[i][1]) == getIndex.end())
+        {
+            List.push_back({make_pair(equations[i][0], (1 / values[i]))});
+            getIndex[equations[i][1]] = List.size() - 1;
+        }
+        else
+            List[getIndex[equations[i][1]]].push_back({equations[i][0], (1 / values[i])});
     }
-    vector<int> *List = obj->getList();
-    DFS(List, n);
+    int val_size = queries.size();
+    vector<double> ans;
+    for (int i = 0; i < val_size; i++)
+    {
+        if (getIndex.find(queries[i][0]) == getIndex.end())
+            ans.push_back(-1);
+        else
+            ans.push_back(dfs(List, getIndex, queries[i][0], queries[i][1], getIndex[queries[i][0]]));
+    }
+    return ans;
 }
