b739f71d2da9e43b730b97b0a81365bb6dd9e965
samarth1345 <76591895+samarth1345@users.noreply.github.com>
2022-02-26 00:48:20 +0530

    //

--- algorithms/DP/subset_sum.cpp
+++ algorithms/DP/subset_sum.cpp
@@ -5,7 +5,7 @@
 #include <queue>
 #include <stack>
 using namespace std;
-//memoization approach
+// memoization approach
 int dp[101][10001];
 bool help(int N, int arr[], int sum)
 {
@@ -22,4 +22,25 @@ bool isSubsetSum(int N, int arr[], int sum)
     // code here
     memset(dp, -1, sizeof(dp));
     return help(N, arr, sum);
+}
+// bottom up approach
+bool dp[101][10001];
+bool isSubsetSum(int N, int arr[], int sum)
+{
+    // code here
+    for (int i = 0; i <= N; i++)
+        dp[i][0] = true;
+    for (int i = 1; i <= sum; i++)
+        dp[0][i] = false;
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= sum; j++)
+        {
+            if (j - arr[i - 1] >= 0)
+                dp[i][j] = (dp[i - 1][j - arr[i - 1]] | dp[i - 1][j]);
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
+    }
+    return dp[N][sum];
 }
