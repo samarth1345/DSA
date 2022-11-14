e5a7712375108896fedec4a1a9faec858869a558
samarth1345 <76591895+samarth1345@users.noreply.github.com>
2022-11-14 21:34:23 +0530

    //

--- Datastructures/graph/bellmanford.cpp
+++ Datastructures/graph/bellmanford.cpp
@@ -1,8 +1,9 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+// bassically this handles negative edges which dijkstras coudld not handle
 vector<int> bellman_ford(int n, vector<vector<int>> edges)
 {
-    //it returns shortest path array and it expects n(no of vertices) and edges vector
+    // it returns shortest path array and it expects n(no of vertices) and edges vector
     vector<int> shortest(n, INT_MAX);
     shortest[0] = 0;
     for (int i = 1; i <= n - 1; i++)
@@ -18,8 +19,8 @@ vector<int> bellman_ford(int n, vector<vector<int>> edges)
             }
         }
     }
-    //if there is a negative cycle it will return empty array
-    //this part checks is there is negative cycle
+    // if there is a negative cycle it will return empty array
+    // this part checks is there is negative cycle
     for (int j = 0; j < edges.size(); j++)
     {
         int u = edges[j][0];
@@ -30,6 +31,6 @@ vector<int> bellman_ford(int n, vector<vector<int>> edges)
             return {};
         }
     }
-    //if there is no negative cycle it will return shortest path array
+    // if there is no negative cycle it will return shortest path array
     return shortest;
 }
