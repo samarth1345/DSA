95c542e0bca90416c13f4a2b826780d53a4b653a
samarth1345 <76591895+samarth1345@users.noreply.github.com>
2022-11-20 01:44:07 +0530

    //

--- algorithms/convex hull/grahams_scan.cpp
+++ algorithms/convex hull/grahams_scan.cpp
@@ -1,59 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
-static bool comp(pair<int,int> a,pair<int,int> b){
-    if(a.first==b.first) return a.second<b.second;
-    return a.first<b.first;
+/*IF COLLINEAR POINTS ARE NOT CONSIDERED INSIDE CONVEX HULL*/
+static bool comp(pair<int, int> a, pair<int, int> b)
+{
+    if (a.first == b.first)
+        return a.second < b.second;
+    return a.first < b.first;
 }
-//check if angle is clockwise
-bool cw(pair<int,int> a,pair<int,int> b,pair<int,int> c){
-    int ax=a.first,ay=a.second,bx=b.first,by=b.second,cx=c.first,cy=c.second;
+bool clockwise(pair<int, int> a, pair<int, int> b, pair<int, int> c)
+{
+    int ax = a.first, ay = a.second, bx = b.first, by = b.second, cx = c.first, cy = c.second;
     return ax * (cy - by) + bx * (ay - cy) + cx * (by - ay) > 0;
 }
-//check if angle is anticlockwise
-bool acw(pair<int,int> a,pair<int,int> b,pair<int,int> c){
-    int ax=a.first,ay=a.second,bx=b.first,by=b.second,cx=c.first,cy=c.second;
+bool anticlockwise(pair<int, int> a, pair<int, int> b, pair<int, int> c)
+{
+    int ax = a.first, ay = a.second, bx = b.first, by = b.second, cx = c.first, cy = c.second;
     return ax * (cy - by) + bx * (ay - cy) + cx * (by - ay) < 0;
 }
-bool collinear(pair<int,int> a,pair<int,int> b,pair<int,int> c){
-    int ax=a.first,ay=a.second,bx=b.first,by=b.second,cx=c.first,cy=c.second;
+bool collinear(pair<int, int> a, pair<int, int> b, pair<int, int> c)
+{
+    int ax = a.first, ay = a.second, bx = b.first, by = b.second, cx = c.first, cy = c.second;
     return ax * (cy - by) + bx * (ay - cy) + cx * (by - ay) == 0;
 }
 vector<pair<int, int>> grahams_scan(vector<pair<int, int>> points)
 {
-    int n=points.size();
-    sort(points.begin(),points.end(),comp);
-    vector<pair<int,int>> up,down;
-    //first point is in both the sets
-    up.push_back(points[0]); down.push_back(points[0]);
-    for(int i=1;i<n;i++){
-        //either it is last point or angle between first,this and last point is clockwise
-        if(i==n-1 || cw(points[0],points[i],points[n-1])){
-            //these are up region points
-            while(up.size()>2 && acw(up[up.size()-2],up[up.size()-1],points[i]))
+    //IF COLLINEAR POINTS ARE CONSIDERED UN-COMMENT LINES 67-72
+    int n = points.size();
+    if (n < 3)
+        return points;
+    sort(points.begin(), points.end(), comp);
+    vector<pair<int, int>> up, down, middle;
+    up.push_back(points[0]);
+    down.push_back(points[0]);
+    for (int i = 1; i < n; i++){
+        if (i == n - 1)
+        {
+            while (up.size() >= 2 && anticlockwise(up[up.size() - 2], up[up.size() - 1], points[i]))
+            {
+                up.pop_back();
+            }
+            while (down.size() >= 2 && clockwise(down[down.size() - 2], down[down.size() - 1], points[i]))
+                down.pop_back();
+            up.push_back(points[i]);
+        }
+        else if (clockwise(points[0], points[i], points[n - 1]))
+        {
+            while (up.size() >= 2 && anticlockwise(up[up.size() - 2], up[up.size() - 1], points[i]))
                 up.pop_back();
             up.push_back(points[i]);
         }
-        else if (i == n - 1 || acw(points[0], points[i], points[n - 1]))
+        else if (anticlockwise(points[0], points[i], points[n - 1]))
         {
-            // these are up region points
-            while (down.size() > 2 && cw(down[down.size() - 2], down[down.size() - 1], points[i]))
+            while (down.size() >= 2 && clockwise(down[down.size() - 2], down[down.size() - 1], points[i]))
                 down.pop_back();
             down.push_back(points[i]);
         }
+        else
+            middle.push_back(points[i]);
     }
     points.clear();
-    for(auto i:up) points.push_back(i);
-    for(auto i:down) points.push_back(i);
-    sort(points.begin(),points.end());
-    //taking only the unique points
-    points.resize(unique(points.begin(),points.end())-points.begin());
+    for (auto i : up)
+        points.push_back(i);
+    for (auto i : down)
+        points.push_back(i);
+    // if ((up.size() == 2 || down.size() == 1))
+    // {
+    //     while (!middle.empty())
+    //         points.push_back(middle.back()), middle.pop_back();
+    // }
+    sort(points.begin(), points.end());
+    points.resize(unique(points.begin(), points.end()) - points.begin());
     return points;
 }
-int main(){
-    int n;
-    cin>>n;
-    vector<pair<int,int>> points(n);
-    for(int i=0;i<n;i++) cin>>points[i].first,cin>>points[i].second;
-    vector<pair<int,int>> ans=grahams_scan(points);
-    for(auto i:ans) cout<<i.first<<" "<<i.second<<endl; 
-}
+/*************************************************************************************/
