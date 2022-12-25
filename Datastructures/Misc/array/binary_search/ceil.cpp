8215d1f25d518f21cd1ead2967603d528037c9c4
samarth1345 <76591895+samarth1345@users.noreply.github.com>
2022-12-25 13:09:20 +0530

    changes

--- Datastructures/Misc/array/binary_search/ceil.cpp
+++ Datastructures/Misc/array/binary_search/ceil.cpp
@@ -1,32 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int findCeil(long long int arr[], int N, long long int K) {
-    
-    //Your code here
-    int start=0,end=N-1,mid;
-    long long int ans=-1;
+int findCeil(vector<int> arr,int k) {
+    int n=arr.size();
+    int start=0,end=n-1,mid,ans=-1;
     while(start<=end)
     {
         mid=start+(end-start)/2;
-        if(arr[mid]==K)
-        return K;
-        else if(arr[mid]<K)
-        start=mid+1;
+        if(arr[mid]==k)
+            return mid;
+        else if(arr[mid]<k)
+            start=mid+1;
         else
         {
-        ans=arr[mid];
-        end=mid-1;
+            ans=mid;
+            end=mid-1;
         }
     }
     return ans;
-}
-int main()
-{
-    int N;
-    cin>>N;
-    long long int arr[N],K;
-    for(int i=0;i<N;i++)
-    cin>>arr[i];
-    cin>>K;
-    cout<<findCeil(arr,N,K);
 }
--- Datastructures/Misc/array/binary_search/floor.cpp
+++ Datastructures/Misc/array/binary_search/floor.cpp
@@ -1,32 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int findFloor(long long int arr[], int N, long long int K) {
-    
-    //Your code here
-    int start=0,end=N-1,mid;
-    long long int ans=-1;
+int floor(vector<int> arr,int K) {
+    int N=arr.size();
+    int start=0,end=N-1,mid,ans=-1;
     while(start<=end)
     {
         mid=start+(end-start)/2;
         if(arr[mid]==K)
-        return K;
+            return mid;
         else if(arr[mid]<K)
         {
-            ans=max(ans,arr[mid]);
+            ans=mid;
             start=mid+1;
         }
         else
-        end=mid-1;
+            end=mid-1;
     }
     return ans;
-}
-int main()
-{
-    int N;
-    cin>>N;
-    long long int arr[N],K;
-    for(int i=0;i<N;i++)
-    cin>>arr[i];
-    cin>>K;
-    cout<<findFloor(arr,N,K);
 }
