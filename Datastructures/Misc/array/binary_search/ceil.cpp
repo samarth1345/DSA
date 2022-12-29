e6ef2eb8f1eebe59be7ecf2c2bf671a43dec45df
samarth1345 <76591895+samarth1345@users.noreply.github.com>
2022-12-29 22:54:40 +0530

    //

--- Datastructures/Misc/array/binary_search/ceil.cpp
+++ Datastructures/Misc/array/binary_search/ceil.cpp
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int ceil(vector<int> arr,int k) {
-    int n=arr.size();
-    int start=0,end=n-1,mid,ans=-1;
-    while(start<=end)
+int ceil(vector<int>& arr, int k)
+{
+    int n = arr.size();
+    int start = 0, end = n - 1, mid, ans = -1;
+    while (start <= end)
     {
-        mid=start+(end-start)/2;
-        if(arr[mid]==k)
+        mid = start + (end - start) / 2;
+        if (arr[mid] == k)
             return mid;
-        else if(arr[mid]<k)
-            start=mid+1;
+        else if (arr[mid] < k)
+            start = mid + 1;
         else
         {
-            ans=mid;
-            end=mid-1;
+            ans = mid;
+            end = mid - 1;
         }
     }
     return ans;
