738cb6d9ea552471df5ba10837ba217ba41fa70c
Samarth Babbar <samarth.babbar@rubrik.com>
2025-09-28 19:32:37 +0530

    Update 2025

--- zcontests/Test2.cpp
+++ zcontests/Test2.cpp
@@ -78,11 +78,6 @@ void solve(){
     }
 }
 int32_t main(){
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-    #endif
-    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     int t=1;
     cin>>t;
     for(int i=1;i<=t;i++){
